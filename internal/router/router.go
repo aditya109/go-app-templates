a32d524da34df92edc0e258a0f36316d3f145e30
Aditya Kumar <adikid1996@gmail.com>
2022-01-30 22:25:52 +0530

    refactoring router pkg; BCH score improvement WIP;
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// ConfigureRouter provides a route-configured *mux.Router object
 func ConfigureRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
@@ -20,6 +21,7 @@ func ConfigureRouter() *mux.Router {
 	return router
 }
 
+// ConfigureHandler enhances a handler function with ServeHTTP
 func ConfigureHandler(inner http.Handler, route Route) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		inner.ServeHTTP(w, r)
